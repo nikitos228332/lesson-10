f2b9b4081811b882b519dc7dc07800bab3659b3c
dolga <dolganov-ninkita@inbox.ru>
2024-02-11 11:20:54 +0300

    first

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.SplittableRandom;
+
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
 
+     String firstName = "Ivan";
+     String middleName = "Ivanovich";
+     String lastName = "Ivanov";
+     String fullName = lastName + " " + firstName + " " + middleName;
+     System.out.println("Ф.И.О. сотрудника - " + fullName);
+
+        System.out.println("Задача 2");
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф.И.О. сотрудника для заполнения отчета - " + fullName);
+        String fullname1 = "Иванов Семён Семёнович";
+
+        System.out.println("Задача 3");
+
+        fullname1 = fullname1.replace('ё', 'е');
+        System.out.println("Даннные Ф.И.О. сотрудника - " + fullname1);
     }
 }
